bc0adf49e7324e19fb25f3d4f9b33d9384f12fd6
th3cyberak <th3cyberak@gmail.com>
2025-05-27 22:43:18 +0530

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
-import GoogleAuth from './GoogleAuth';
+class Header extends React.Component {
+  renderAuthButton() {
+    if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.props.signOut} className="ui red button">
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.props.signIn} className="ui green button">
+          Sign In
+        </button>
+      );
+    }
+  }
 
-const Header = () => {
-  return (
-    <div className="ui secondary pointing menu">
-      <Link to="/" className="item">
-        Streamy
-      </Link>
-      <div className="right menu">
+  render() {
+    return (
+      <div className="ui secondary pointing menu">
         <Link to="/" className="item">
-          All Streams
+          Streamy
         </Link>
-        <GoogleAuth />
+        <div className="right menu">
+          {this.renderAuthButton()}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
 };
 
-export default Header;
+export default connect(mapStateToProps, { signIn, signOut })(Header);
